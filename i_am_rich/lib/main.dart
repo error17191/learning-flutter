da2070b8394f747d016a7b323f0eba6a9c2932c0
Mohamed Ahmed <error17191@gmail.com>
2019-11-20 17:23:56 +0200

    IamRichApp Done

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -1,5 +1,30 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Text("Hello World")
-));
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              "Iam Rich",
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+          backgroundColor: Colors.blueGrey,
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage(
+              'images/diamond.png',
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
